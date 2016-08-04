a731590a69bb81b5fb8c9b0e5e3e1bd6213e1312
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-04 14:04:41 -0400

    Added code to keep track of total connected users, added join section to join under specific name, updated message section to show who is sending message, updated disconnect section to remove user once disconnected in server.js.

--- server.js
+++ server.js
@@ -10,18 +10,47 @@ app.use(express.static('public'));
 var server = http.Server(app);
 //create sockets
 var io = socket_io(server);
+var usersConnected = 0;
+var users = {};
 //attach listener to connection event
 io.on('connection', function(socket) {
   //log message to the console
   console.log('Client connected');
+  //increment usersconnected count
+  usersConnected++;
+  console.log('Users connected: ' + usersConnected);
+  //pass message to browser when a user connects
+  socket.broadcast.emit('message', 'user connected');
+  //attach listener that will allow user to be assigned a name
+  socket.on('join', function(name) {
+    users[socket.id] = name;
+    socket.emit("update", "You are connected to the server.");
+    socket.broadcast.emit("update", name + " has joined the server.")
+    socket.broadcast.emit("update-users", users);
+
+  });
+  socket.on('send', function(message){
+        socket.broadcast.emit("message", users[socket.id], msg);
+    });
   //attach listener for message event
   socket.on('message', function(message) {
     //log message to the console
-    console.log('Received message:', message);
+    console.log('Received message:', message + 'from user: ' + user[socket.id]);
     //send message to all clients except starting socket
-    socket.broadcast.emit('message', message);
+    socket.broadcast.emit("message", users[socket.id], msg);
+  });
+  //send message to users when a user disconnects
+  socket.on('disconnect', function() {
+    usersConnected--;
+    console.log('Users connected: ' + usersConnected);
+    socket.broadcast.emit("update", users[socket.id] + " has disconnected.");
+    delete users[socket.id];
+    socket.broadcast.emit("update-users", users);
+    //send message when a user disconnects
+    socket.broadcast.emit('message', 'user disconnected');
   });
 });
+
 
 //start server listening on port
 server.listen(8080);
