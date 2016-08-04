ac1c59de5ea208373b5b493b41dec9568b491f9d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-04 10:56:09 -0400

    Added minor documentation to main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
+  //select input tag
   var input = $('input');
+  //get div with messages id
   var messages = $('#messages');
-
+  //add message contents to the screen
   var addMessage = function(message) {
     messages.append('<div>' + message + '</div>');
   };
+  //function to listen to keydown when enter is pressed
   input.on('keydown', function(event){
+    //if the key is not return
     if (event.keyCode != 13) {
+      //exit out
       return;
     }
+    //get the inputs contents
     var message = input.val();
+    //call the addmessage function passing the input contents
     addMessage(message);
+    //empty input
     input.val('');
   });
 });
