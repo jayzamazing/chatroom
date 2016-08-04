47f7d634d4ade4b98f384a229329271ab157b186
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-04 11:48:05 -0400

    Added socket.io to server.js and initialization code for it.

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+//get required modules
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
+//get instance of express
 var app = express();
+//add static files for express to use
 app.use(express.static('public'));
-app.listen(8080);
+//create http server, allows express to run at same time as socket.io
+var server = http.Server(app);
+//create sockets
+var io = socket_io(server);
+//attach listener to connection event
+io.on('connection', function(socket) {
+  console.log('Client connected');
+});
+
+//start server listening on port
+server.listen(8080);
